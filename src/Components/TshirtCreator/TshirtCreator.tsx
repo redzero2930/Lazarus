58e2337900903dc3da2b5581042605e83c48dedf
redzero2930 <redzero2930@gmail.com>
2024-10-09 17:45:00 -0700

    Primer commit

---
+++ src/Components/TshirtCreator/TshirtCreator.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TshirtCreator = () => {
+    return (
+        <div>
+            <h1>Bienvenido a tu creador de camisetas</h1>
+        </div>
+    );
+};
+
+export default TshirtCreator;
